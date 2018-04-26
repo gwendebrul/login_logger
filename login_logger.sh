8a90bdd461e6301238dc2f3ae04f392e1c8f4604
Gwen De Brul <gwen@debrul.be>
2018-04-26 14:14:15 +0200

    Working script

---
+++ login_logger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source .env
+IP=$(echo $SSH_CLIENT|grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+SERVERNAME=$(hostname)
+USERNAME=$(whoami)
+LOGIN_DATE=$(date +'%A %d %B, %H:%M')
+
+LOGGED_STRING="$USERNAME logged in on $SERVERNAME on $LOGIN_DATE from $IP"
+
+curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"$LOGGED_STRING\"}" $IFTTT_URL
