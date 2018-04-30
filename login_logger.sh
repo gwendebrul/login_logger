33950d01bdc12f182078bd0fb7d908c99bbf3fd0
Gwen De Brul <gwen@debrul.be>
2018-04-30 12:49:23 +0200

    Clear trap signals at end of script.

--- login_logger.sh
+++ login_logger.sh
@@ -11,3 +11,6 @@ LOGIN_DATE=$(date +'%A %d %B, %H:%M')
 LOGGED_STRING="$USERNAME logged in on $SERVERNAME on $LOGIN_DATE from $IP"
 
 curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"$LOGGED_STRING\"}" $IFTTT_URL
+
+trap - 1 2 3 15
+trap
